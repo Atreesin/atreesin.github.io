dd5f7940d705fe1ce0d007a23467e0accb4ce885
fernando ariel franco <82460657+Atreesin@users.noreply.github.com>
2023-02-24 16:59:55 -0300

    Update program.js

--- recursos/program.js
+++ recursos/program.js
@@ -2,4 +2,4 @@
 import informacion from 'https://atreesin.github.io/recursos/informacion.json' assert {type: 'json'};
 
 const info = informacion.persona[0].foto_perfil.large
-document.getElementById("foto_perfil").setAttribute('src', '/recursos/'+info);
+document.getElementById("foto_perfil").setAttribute('src', './recursos/'+info);
