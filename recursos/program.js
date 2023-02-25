85f01421f3690991047f069c9250a40e15b95d87
fernando ariel franco <82460657+Atreesin@users.noreply.github.com>
2023-02-25 16:58:04 -0300

    Add files via upload

--- recursos/program.js
+++ recursos/program.js
@@ -4,9 +4,7 @@ import informacion from './informacion.json' assert {type: 'json'};
 
 const info = informacion
 
-console.log(info.persona[3].habilidades);
-
-
+console.log(info.persona[0].idiomas.length)
 
 /* codigo para seleccionar y actualizar datos de curriculum con un selector */
 const selectorPerfiles = document.querySelector('.perfiles');
@@ -22,6 +20,7 @@ selectorPerfiles.addEventListener('change', (event) => {
 
 
 })
+
 
 
 
@@ -51,6 +50,25 @@ function actualizarDatos(perfilList, nPerfil) {
     actualizarHabilidades(p, n);
     actualizarSoftware(p, n);
     actualizarIdiomas(p, n);
+
+    document.getElementById("perfil").innerHTML = p.persona[n].perfil_laboral
+
+    actualizarExperiencia(p, n);
+    actualizarEducacion(p, n);
+    
+    actualizarCertificados(p,n);
+
+    if (p.persona[n].objetivos_personales != false) {
+        document.getElementById("objetivos").innerHTML = p.persona[n].objetivos_personales
+
+        document.getElementById("seccion_objetivos").style.display = "block"
+
+    }
+    else {
+        document.getElementById("seccion_objetivos").style.display = "none"
+
+    }
+
 
 }
 
@@ -86,6 +104,17 @@ function actualizarFotoSmall(perfilList, nPerfil) {
 
 
 
+/** 
+ * 
+ * 
+ * funcion para eliminar los elementos
+ * de determinada lista desordenada
+ * y luego para agregar items
+ * 
+ *  
+ * 
+ * 
+*/
 
 
 function actualizarHabilidades(perfil, n) {
@@ -95,8 +124,10 @@ function actualizarHabilidades(perfil, n) {
 
     if (perfil.persona[n].habilidades.length != 0) {
 
+        document.getElementById("seccion_habilidades").style.display = "block"
 
         for (let i = 0; i < perfil.persona[n].habilidades.length; i++) {
+            console.log(i + "hab");
             var habilidad = perfil.persona[n].habilidades[i].habilidad
             var nivel = perfil.persona[n].habilidades[i].nivel
             add_li(habilidad, nivel, "habilidades");
@@ -106,10 +137,11 @@ function actualizarHabilidades(perfil, n) {
 
 
     } else {
-        console.log("no añadio habilidades")
+        document.getElementById("seccion_habilidades").style.display = "none"
     }
 
 }
+console.log(info.persona[0].software[0].software)
 
 function actualizarSoftware(perfil, n) {
 
@@ -118,18 +150,21 @@ function actualizarSoftware(perfil, n) {
 
     if (perfil.persona[n].software.length != 0) {
 
+        document.getElementById("seccion_software").style.display = "block"
 
         for (let i = 0; i < perfil.persona[n].software.length; i++) {
+            console.log(i + "soft");
             var software = perfil.persona[n].software[i].software
             var nivel = perfil.persona[n].software[i].nivel
             add_li(software, nivel, "software");
+
         }
 
 
 
 
     } else {
-        console.log("no añadio habilidades")
+        document.getElementById("seccion_software").style.display = "none"
     }
 
 }
@@ -139,34 +174,138 @@ function actualizarIdiomas(perfil, n) {
 
     eliminar_li("idiomas");
 
-    if (perfil.persona[n].software.length != 0) {
+    if (perfil.persona[n].idiomas.length != 0) {
 
+        document.getElementById("seccion_idiomas").style.display = "block"
 
         for (let i = 0; i < perfil.persona[n].idiomas.length; i++) {
+            console.log(info.persona[n].idiomas.length + "papu")
+            console.log(i + "idioma" + n);
             var idiomas = perfil.persona[n].idiomas[i].idioma
             var nivel = perfil.persona[n].idiomas[i].nivel
             add_li(idiomas, nivel, "idiomas");
+
         }
 
 
 
 
     } else {
-        console.log("no añadio habilidades")
+        document.getElementById("seccion_idiomas").style.display = "none"
     }
 
 }
+
+/**
+ * 
+ * funciones para
+ * Actualizar seccion de experiencia laboral
+ * 
+ * 
+ */
+
+function actualizarExperiencia(perfil, n) {
+
+    /**
+     * tengo que crear una funcion que me cree items
+     * en una lista desordenada
+     * y debe contener un articule class= "experiencia_laboral_item"
+     * dentro del article con p id="fecha"
+     * h3 puesto ocupado
+     * <p><span id="empresa">Empresa</span> <br> descripcion</p>
+     * 
+     */
+
+    eliminar_li("experiencia_laboral");
+
+    info.persona[0].experiencia[1].descripcion
+    if (perfil.persona[n].experiencia.length != 0) {
+
+        document.getElementById("seccion_experiencia").style.display = "block"
+
+        for (let i = 0; i < perfil.persona[n].experiencia.length; i++) {
+
+
+            var fecha_inicial = perfil.persona[n].experiencia[i].fecha_inicio
+            var fecha_final = perfil.persona[n].experiencia[i].fecha_fin
+            var puesto = perfil.persona[n].experiencia[i].puesto
+            var empresa = perfil.persona[n].experiencia[i].empresa
+            var descripcion = perfil.persona[n].experiencia[i].descripcion
+            add_exp(fecha_inicial, fecha_final, puesto, empresa, descripcion, "experiencia_laboral");
+
+        }
+
+
+
+
+    } else {
+        document.getElementById("seccion_experiencia").style.display = "none"
+    }
+
+
+
+}
+
+
+
+function actualizarEducacion(perfil, n) {
+
+    eliminar_li("educacion");
+
+
+    if (perfil.persona[n].educacion.length != 0) {
+
+        document.getElementById("seccion_educacion").style.display = "block"
+
+        for (let i = 0; i < perfil.persona[n].educacion.length; i++) {
+
+
+
+            var fecha_inicial = perfil.persona[n].educacion[i].fecha_inicio
+            var fecha_final = perfil.persona[n].educacion[i].fecha_fin
+            var titulo = perfil.persona[n].educacion[i].titulo
+            var institucion = perfil.persona[n].educacion[i].instituto
+            var ciudad = perfil.persona[n].educacion[i].ciudad
+            add_educ(fecha_inicial, fecha_final, titulo, institucion, ciudad, "educacion");
+
+        }
+
+
+
+
+    } else {
+        document.getElementById("seccion_educacion").style.display = "none"
+    }
+
+
+}
+
+
+
+
+function actualizarCertificados(p, n) {
+
+    add_cert(p, n, "id");
+
+}
+
+
+
+
+
+
+
 
 
 /**
  * 
  * 
- * test
+ * funcion para agregar items a una lista desordenada
  * 
  * 
  */
 
-console.log(info.persona[3].habilidades[1].habilidad);
+
 
 function add_li(h, v, id) {
     var nuevoLi = h;
@@ -175,7 +314,7 @@ function add_li(h, v, id) {
         if (find_li(nuevoLi, id)) {
             var li = document.createElement('li');
             li.id = id;
-            li.innerHTML = '<p id= "'+id+'">' + h + '</p> <p id="valor">' + v + "<p>";
+            li.innerHTML = '<p id= "' + id + '">' + h + '</p> <p id="valor">' + v + "<p>";
             document.getElementById(id).appendChild(li);
         }
     }
@@ -197,19 +336,103 @@ function find_li(contenido, id) {
 
 /**
  * Funcion para eliminar los elementos
- * Tiene que recibir el elemento pulsado
+ * hijos de un nodo
+ * recibe un id del nodo padre
  */
 function eliminar_li(id) {
-    
+
     const hijos = document.getElementById(id).childNodes.length;
     const padre = document.getElementById(id);
 
     for (let i = 0; i < hijos; i++) {
 
-        console.log(hijos + "swegf");
+
         const temp = padre.firstChild
         document.getElementById(id).removeChild(temp)
 
     }
 
 }
+
+
+/**
+ * 
+ * funciones para agregar
+ * items a experiencia laboral
+ * 
+ */
+
+function add_exp(fini, ffin, pues, emp, desc, id) {
+    var nuevoLi = emp;
+    let fechaini = fini;
+    let fechafin = ffin;
+
+
+
+
+    if (nuevoLi.length > 0) {
+        if (find_li(nuevoLi, id)) {
+            var li = document.createElement('li');
+            li.id = id;
+
+            li.innerHTML = '<article class="esperiencia_laboral_item"><p id="fecha">' + fechaini + " " + fechafin + '</p><h3 id="puesto">' + pues + '</h3><p><span id="empresa">' + emp + "</span> <br> " + desc + "</p>";
+            document.getElementById(id).appendChild(li);
+        }
+    }
+    return false;
+}
+
+
+function add_educ(fini, ffin, tit, inst, ciu, id) {
+    var nuevoLi = tit;
+    let fechaini = fini;
+    let fechafin = ffin;
+
+    if (nuevoLi.length > 0) {
+        if (find_li(nuevoLi, id)) {
+            var li = document.createElement('li');
+            li.id = id;
+            if (ciu != false) {
+                li.innerHTML = '<article class="educacion_item"><h3 id="titulo">' + tit + '</h3><p id="fecha">' + fechaini + " " + fechafin + '</p><span id="instituto">' + inst + ", " + ciu;
+            }
+            else {
+                li.innerHTML = '<article class="educacion_item"><h3 id="titulo">' + tit + '</h3><p id="fecha">' + fechaini + " " + fechafin + '</p><span id="instituto">' + inst + ".";
+
+            }
+            document.getElementById(id).appendChild(li);
+        }
+    }
+    return false;
+
+}
+
+function add_cert(esp, inst, id) {
+    let nuevoLi = esp;
+    let fechaini = inst;
+    /*
+
+    if (nuevoLi.length > 0) {
+        if (find_li(nuevoLi, id)) {
+            var li = document.createElement('li');
+            li.id = id;
+            if(ciu != false){
+                li.innerHTML = '<article class="educacion_item"><h3 id="titulo">'+tit+'</h3><p id="fecha">'+fechaini+" "+fechafin+'</p><span id="instituto">'+inst+", "+ciu;
+            }
+            else{
+                li.innerHTML = '<article class="educacion_item"><h3 id="titulo">'+tit+'</h3><p id="fecha">'+fechaini+" "+fechafin+'</p><span id="instituto">'+inst+".";
+            
+            }
+            document.getElementById(id).appendChild(li);
+        }
+    }
+    return false;
+    */
+    let node = [esp, inst]
+    
+
+        document.getElementById("certificados").append(node)
+
+
+}
+
+
