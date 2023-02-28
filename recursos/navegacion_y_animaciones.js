7961f850dca80d62c5460db7304ca065722bae76
fernando ariel franco <82460657+Atreesin@users.noreply.github.com>
2023-02-28 19:55:11 -0300

    Add files via upload

--- recursos/navegacion_y_animaciones.js
+++ recursos/navegacion_y_animaciones.js
@@ -1,10 +1,170 @@
 barras = document.querySelector(".bars");
+
+adicional = document.querySelector(".adicional");
+
+
+
+btnhabilidades = document.getElementById("link_habilidades");
+btnsoftware = document.getElementById("link_software");
+btnidiomas = document.getElementById("link_idiomas");
+
+infoad_oculto = true;
+infohab_oculto = true;
+infosoft_oculto = true;
+infoidi_oculto = true;
+
+infoadicional = document.querySelector(".info_adicional")
+infohabilidades = document.querySelector(".info_habilidades")
+infosoftware = document.querySelector(".info_software")
+infoidiomas = document.querySelector(".info_idiomas")
+
 barras.onclick = function () {
     navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active")
+    if (infoad_oculto) {
+        navBar.classList.toggle("active");
+    } else {
+        ocultarinfo();
+        navBar.classList.toggle("active")
+    }
+
 }
-adicional = document.querySelector(".adicional");
-adicional.onclick = function () {
-    infoadicional = document.querySelector(".info_adicional")
-    infoadicional.classList.toggle("active")
+
+ 
+function ocultarinfo(){
+    console.log("seoculto");
+
+    if(!infoad_oculto){
+        infoadicional.classList.toggle("active");
+        infoad_oculto = true;
+        if(!infohab_oculto){
+            infohabilidades.classList.toggle("active");
+            infohab_oculto = true            
+        }
+        if(!infosoft_oculto){
+            infosoftware.classList.toggle("active");
+            infosoft_oculto = true
+        }
+        if(!infoidi_oculto){
+            infoidiomas.classList.toggle("active");
+            infoidi_oculto = true
+        }
+    }
+
+    
+    
 }
+
+
+
+btnhabilidades.onclick = function () {
+    
+
+    if(infohab_oculto){
+        if(infoad_oculto){
+            infoadicional.classList.toggle("active");
+            infoad_oculto = false;
+            infohabilidades.classList.toggle("active");
+            infohab_oculto = false            
+        }else{
+            
+            if(!infosoft_oculto){
+                infosoftware.classList.toggle("active");
+                infosoft_oculto = true
+            }
+            if(!infoidi_oculto){
+                infoidiomas.classList.toggle("active");
+                infoidi_oculto = true
+            }
+
+            infohabilidades.classList.toggle("active");
+            infohab_oculto = false
+        }
+    }else{
+        
+        ocultarinfo();
+    }
+    
+    
+    
+    
+}
+
+btnsoftware.onclick = function () {
+    
+    if(infosoft_oculto){
+        if(infoad_oculto){
+            infoadicional.classList.toggle("active");
+            infoad_oculto = false;
+            infosoftware.classList.toggle("active");
+            infosoft_oculto = false            
+        }else{
+            
+            if(!infohab_oculto){
+                infohabilidades.classList.toggle("active");
+                infohab_oculto = true
+            }
+            if(!infoidi_oculto){
+                infoidiomas.classList.toggle("active");
+                infoidi_oculto = true
+            }
+
+            infosoftware.classList.toggle("active");
+            infosoft_oculto = false
+        }
+    }else{
+        
+        ocultarinfo();
+    }
+
+}
+
+btnidiomas.onclick = function () {
+    if(infoidi_oculto){
+        if(infoad_oculto){
+            infoadicional.classList.toggle("active");
+            infoad_oculto = false;
+            infoidiomas.classList.toggle("active");
+            infoidi_oculto = false            
+        }else{
+            
+            if(!infosoft_oculto){
+                infosoftware.classList.toggle("active");
+                infosoft_oculto = true
+            }
+            if(!infohab_oculto){
+                infohabilidades.classList.toggle("active");
+                infohab_oculto = true
+            }
+
+            infoidiomas.classList.toggle("active");
+            infoidi_oculto = false
+        }
+    }else{
+        
+        ocultarinfo();
+    }
+   
+}
+
+
+const btnEl = document.querySelector('.adicional')
+const toggleOptions = () => {
+    const wrapperEl = document.querySelector('.wrapper')
+    const iconEl = btnEl.querySelector('svg')
+
+    wrapperEl.classList.toggle('active')
+
+    if (iconEl.classList.contains('fa-plus')) {
+        iconEl.classList.replace('fa-plus', 'fa-minus')
+    } else {
+        iconEl.classList.replace('fa-minus', 'fa-plus')
+        ocultarinfo()
+    }
+
+
+
+}
+btnEl.addEventListener('click', toggleOptions)
+
+
+
